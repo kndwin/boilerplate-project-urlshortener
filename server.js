e722c454797e765920eae452dc6097599629108e
kndwin <tnkevino@gmail.com>
2020-04-13 17:49:12 +1000

    got POST and GET to return something at API endpoint, set up mongoDB (connected) and schema set up, set up .env (working) and added .gitignore for node_modules

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var cors = require('cors');
 
@@ -13,11 +14,37 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.DB_URI);
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const Schema = mongoose.Schema;
+const urlSchema = new Schema ({
+    urlGiven: {
+        type: String,
+        required: true
+    },
+    urlShortened: String
+});
+const Url = mongoose.model('Url', urlSchema);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(
+    bodyParser.urlencoded({ extended: false  }),
+    bodyParser.json()
+);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,6 +58,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.route("/api/shorturl/new").get(function(req, res){
+    res.json({ "url": req.query })
+}).post(function(req, res){
+    console.log(req.body)
+    res.json({ "url": req.body })
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
